c57d000eece6f53f1fe8337ed34f549256d023d1
Hanna Engel <hengel@alumni.berklee.edu>
2019-06-11 12:35:30 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Your code here
+class Cat {
+    constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+    }
+
+    speak(){
+        let name = this.name 
+        return `${name} says meow!`
+    }
+
+  }
+   
+  class Dog {
+    constructor(name, sex){
+        this.name = name;
+        this.sex = sex;
+        }
+        speak(){
+            let name = this.name 
+            return `${name} says woof!`
+        }
+      }
+
+    class Bird {
+    constructor(name, sex){
+        this.name = name;
+        this.sex = sex;
+        }
+
+            speak(){
+                
+            let name = this.name 
+                if(this.sex==='male'){
+            return `It's me! ${name}, the parrot!`
+            }else{
+                return `${name} says squawk!`
+            }
+            }
+       
+        }
